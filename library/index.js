17b5a62dc538621c3328c22918e0eff96172044f
undefined64 <kirillpretty@gmail.com>
2023-12-18 00:34:16 +0300

    feat: implement menu for mobile devices

--- library/index.js
+++ library/index.js
@@ -1,31 +1,37 @@
 const menuBtn = document.querySelector('.nav__btn');
 const menu = document.querySelector('.nav__list');
-const navBtn = document.querySelector('.nav__btn');
 const navBtnLines = document.querySelectorAll('.nav__btn-line');
 
 menuBtn.addEventListener('click', () => {
-  menuToggle();
-  menuCrossToggle();
+  let menuStyle = window.getComputedStyle(menu).getPropertyValue('display');
+  menuToggle(menuStyle);
+  menuCrossToggle(menuStyle);
 });
 
-const menuToggle = () => {
-  if (menu.style.display === 'none' || menu.style.display === '') {
+const menuToggle = (menuStyle) => {
+  if (menuStyle === 'none') {
     menu.style.display = 'block';
   } else {
     menu.style.display = 'none';
   }
 };
 
-const menuCrossToggle = () => {
-  if (menu.style.display === 'none' || menu.style.display === '') {
+const menuCrossToggle = (menuStyle) => {
+  if (menuStyle == 'none') {
     navBtnLines[0].style.display = 'none';
     navBtnLines[1].style.transform = 'rotate(45deg)';
     navBtnLines[2].style.transform = 'rotate(-45deg)';
-    navBtn.style.transform = 'translateY(10px)';
   } else {
     navBtnLines[0].style.display = 'block';
     navBtnLines[1].style.transform = 'rotate(0deg)';
     navBtnLines[2].style.transform = 'rotate(0deg)';
-    navBtnLines[2].style.transform = 'translateY(-10px)';
   }
 };
+
+menu.addEventListener('click', (event) => {
+  console.log(event.target.tagName);
+  if (event.target.tagName === 'A') {
+    menuToggle();
+    menuCrossToggle();
+  }
+});
